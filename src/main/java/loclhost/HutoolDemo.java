665ece56f90d0a555dcc575af2f766d7e7299d4b
Lady · Lady <gp095@@hotmail.com>
2018-01-16 10:08:28 +0800

    2018年1月16日10点08分 移除了一些Demo配置

--- src/main/java/loclhost/HutoolDemo.java
+++
@@ -1,7 +0,0 @@
-package loclhost;
-
-/**
- * Created by BlueSky on 2017/11/7.
- */
-public class HutoolDemo {
-}
--- src/main/java/loclhost/Java8DateDemo.java
+++
@@ -1,9 +0,0 @@
-package loclhost;
-
-/**
- * Created by BlueSky on 2017/9/13.
- */
-public class Java8DateDemo {
-
-
-}
